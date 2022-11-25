92be59bf9e4ef4a7d7593b9f683fa06308ef2d80
Наталія Прокопенко <100276704+halfbloodhp@users.noreply.github.com>
2022-11-25 16:14:03 +0000

    add header/content/footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
+import * as React from "react"
 import './App.css'
+import Header from './header'
+import Content from './content'
+import Footer from './footer'
+
 
 export default function App() {
   return (
     <main>
-     Hello world!
+    <Header/>
+    <Content/>
+    <Footer/>
     </main>
   )
 }
